92ad4dfb3feecaf112e84259cc53e6d51c575b85
Thanh Phong Le <ltphongssvn@gmail.com>
2026-02-17 19:04:36 -0800

    feat: add services barrel export

---
+++ src/services/index.ts
@@ -0,0 +1,4 @@
+// autoloan-nextjs-metafullstack/src/services/index.ts
+export { apiFetch, setAuthToken, getAuthToken } from './api';
+export { authService } from './auth';
+export { applicationsService } from './applications';
